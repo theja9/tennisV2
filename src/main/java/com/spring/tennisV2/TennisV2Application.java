bbed99380ad16f157e2fd0086cffd19a11399252
Thejashree <thejashreehk@gmail.com>
2023-02-28 11:12:52 +0530

    initial commit

---
+++ src/main/java/com/spring/tennisV2/TennisV2Application.java
@@ -0,0 +1,13 @@
+package com.spring.tennisV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/tennisV2/TennisV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.tennisV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
